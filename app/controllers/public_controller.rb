29e6a46f7e1483c650c720d707f7393082d9930e
michel <michel.valenzuela.bravo@gmail.com>
2022-04-11 18:46:53 -0400

    Adjusting some views,routes, adding some methods on controller

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -4,5 +4,10 @@ class PublicController < ApplicationController
         @communities = Community.all.limit(5)
         @posts = Post.limit(20).sort_by{ |p| p.score }.reverse
     end
+
+    def profile
+        @profile = Account.find_by_username params[:username]
+        @posts = @profile.posts
+    end
    
 end
