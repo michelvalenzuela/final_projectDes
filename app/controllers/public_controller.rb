dd94b4d2cd2f8dd2ee08181439b402112cef5036
michel <michel.valenzuela.bravo@gmail.com>
2022-04-11 21:01:50 -0400

    added subscribers to communities and comments to post..

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,9 +2,8 @@ class PublicController < ApplicationController
 
     def index
         @communities = Community.all.limit(5)
-        @posts = Post.limit(20).sort_by{ |p| p.score }.reverse
+        @posts = Post.limit(20)
     end
-
     def profile
         @profile = Account.find_by_username params[:username]
         @posts = @profile.posts
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   devise_for :accounts
   get "u/:username" => "public#profile", as: :profile
+  
   resources :communities do
     resources :posts
   end
+  resources :subscriptions
+  resources :comments, only: [:create]
+
+  post "post/vote" => "votes#create"
   root to: "public#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
