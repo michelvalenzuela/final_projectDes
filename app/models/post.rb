528faf42e44af91f628fe3783682edde482cfd96
michel <michel.valenzuela.bravo@gmail.com>
2022-04-12 23:25:28 -0400

    adding votes feature for post, also karma that users have.. (with their respective views via ajax,jquery..)

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ApplicationRecord
     belongs_to :community
     validates_presence_of :title, :body, :account_id, :community_id
     has_many :comments
-    before_action :auth_subscriber, only: [:new]
   
     def score
       # difference between upvotes and downvotes
